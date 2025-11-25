facf69bde3684c0c092952b06ce06f9d38e7fb7e
salmansaeedch008-ai <salmansaeedch008@gmail.com>
2025-11-25 22:17:19 +0500

    created function to calculate the total bill of the user . in function we pass an array of prices of all the products , and array of the number of quantities of products , and the total number of products , it will return the total bill of customer.

--- CS_101_project.cpp
+++ CS_101_project.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
 
+float calculate_total(float price[], int quantity[], int n) {
+    float total = 0;
+    for (int i = 0; i < n; i++) {
+        total += price[i] * quantity[i];
+    }
+    return total;
+}
+
 int main(){
+
 
 
     return 0;
